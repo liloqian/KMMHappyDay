ef8ec745175de4bf01722e32596e5c73f3eae9fe
qian <1768031241@qq.com>
2023-08-20 10:47:37 +0800

    feat: ide auto generate

---
+++ android/src/main/java/com/happy/android/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.happy.android
+
+import com.happy.common.App
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                App()
+            }
+        }
+    }
+}
---
+++ common/src/desktopMain/kotlin/com/happy/common/platform.kt
@@ -0,0 +1,5 @@
+package com.happy.common
+
+actual fun getPlatformName(): String {
+    return "Desktop"
+}
